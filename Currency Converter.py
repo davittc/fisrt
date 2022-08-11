fda58f0ee8d4456cccf531ac99b3d94e97fdafcc
davittc <davittc@stu.otc.school.nz>
2022-08-11 13:05:59 +1200

    First code in GUI currency converter

--- Currency Converter.py
+++ Currency Converter.py
@@ -3,10 +3,22 @@ from tkinter import ttk
 Window = Tk()
 Window.title("Currency Converter")
 
+#def to_lanuage_seletor():
+
+#def to_currency_converter():
+
+#def to_references():
+
+def quit_button():
+    quit()
+
+
+
 def test():
     test = ttk.Button(Window, text='yes')
     test.pack()
 
+quit_button = ttk.Button(Window, command=quit_button())
 test = ttk.Button(Window, text='yes', command=test)
 test.pack()
 Window.mainloop()
